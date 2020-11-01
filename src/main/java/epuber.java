2084b9ac64b7684764b5bdfc52c61da8d012dddc
Nikolche Kolevski <nikolche.kolevski@outlook.com>
2020-11-01 22:21:46 +0100

    initial jepuber commit

---
+++ src/main/java/epuber.java
@@ -0,0 +1,57 @@
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.TextPosition;
+
+import java.io.*;
+import java.util.List;
+
+public class epuber extends PDFTextStripper{
+
+    public epuber() throws IOException {
+    }
+
+    public static void main(String[] args) {
+        File file = new File(epuber.class.getClassLoader().getResource("book_for_wine.pdf").getFile());
+        try {
+            //PDDocument cosDoc = PDDocument.load(file);
+            PDDocument document = PDDocument.load(file);
+            PDFTextStripper stripper = new epuber();
+            stripper.setSortByPosition( true );
+            stripper.setStartPage( 0 );
+            stripper.setEndPage( document.getNumberOfPages() );
+            Writer dummy = new OutputStreamWriter(new ByteArrayOutputStream());
+            stripper.writeText(document, dummy);
+            System.out.println(dummy);
+
+            /*
+            *         file = new File(filePath);
+        parser = new PDFParser(new RandomAccessFile(file, "r")); // update for PDFBox V 2.0
+
+        parser.parse();
+        cosDoc = parser.getDocument();
+        pdfStripper = new PDFTextStripper();
+        pdDoc = new PDDocument(cosDoc);
+        pdDoc.getNumberOfPages();
+        pdfStripper.setStartPage(0);
+        pdfStripper.setEndPage(pdDoc.getNumberOfPages());
+        Text = pdfStripper.getText(pdDoc);
+        return Text;
+            * */
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void writeString(String string, List<TextPosition> textPositions) throws IOException {
+        for (TextPosition text : textPositions) {
+            System.out.println(text.getUnicode()+ " [(X=" + text.getXDirAdj() + ",Y=" +
+                    text.getYDirAdj() + ") height=" + text.getHeightDir() + " width=" +
+                    text.getWidthDirAdj() + "]");
+        }
+    }
+}
+
